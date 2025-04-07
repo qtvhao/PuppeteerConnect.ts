f3b367672709e887da760bf7347ee0e246be09ae
Hao Nghiem <qtvhao@gmail.com>
2025-04-07 16:42:31 +0700

    Add

---
+++ src/Utils/UploadFile.ts
@@ -0,0 +1,9 @@
+export class UploadFile {
+    async uploadFile(page: any, url: string, selector: string, file: string) {
+        await page.goto(url);
+
+        await page.waitForSelector(selector);
+        let fileInput = await page.$(selector);
+        await fileInput.uploadFile(file);
+    }
+}
