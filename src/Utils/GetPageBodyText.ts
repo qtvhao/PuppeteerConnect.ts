dac1726160f851e531f5c607d00d708aca5ac538
Hao Nghiem <qtvhao@gmail.com>
2025-04-19 18:04:27 +0700

    Add

--- src/Utils/GetPageBodyText.ts
+++ src/Utils/GetPageBodyText.ts
@@ -1,6 +1,6 @@
 import { Page } from "puppeteer";
 export class GetPageBodyText {
     public static async getInnerText(page: Page): Promise<string> {
-        return await page.evaluate(() => document.body.innerText || "");
+        return await page.evaluate(() => (document.body || {innerText: ''}).innerText || "");
     }
 }
